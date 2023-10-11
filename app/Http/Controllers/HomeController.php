9cdd78b1ca4ab9435d68d5e55f66d10c054e5798
zoe1017zoe <z1017zoe@gmail.com>
2023-10-11 19:16:05 +0800

    此處填寫版本資訊

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,72 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class HomeController extends Controller
+{
+    public function chartEventStream()
+    {
+        // 連線到資料庫
+        DB::connection('mysql');
+
+        $data = [
+            $t = strtotime('+8 hours'),
+            'time' => date('Y-m-d H:i:s', $t),
+
+            // 取值
+            'value' => DB::table('chart')->orderBy('id', 'desc')->limit(1)->value('value')
+        ];
+
+        $response = new StreamedResponse();
+        $response->setCallback(function () use ($data){
+            echo 'data: ' . json_encode($data) . "\n\n";
+            echo "retry: 1000\n";
+            ob_flush();
+            flush();
+        });
+
+        $response->headers->set('Content-Type', 'text/event-stream');
+        $response->headers->set('X-Accel-Buffering', 'no');
+        $response->headers->set('Cach-Control', 'no-cache');
+        $response->send();
+    }
+
+    public function chart()
+    {
+        // 連線到資料庫
+        DB::connection('mysql');
+
+        // 取值
+        $value = DB::table('chart')->orderBy('id', 'desc')->limit(1)->value('value');
+
+        return view('chart')->with('value',$value);
+    }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ resources/views/chart.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.css"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css"></script>
+                
+<!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+           
+            <div>
+                <canvas id="myChart" width="600" height="600"></canvas>
+                <script>
+                    var ctx = document.getElementById('myChart').getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            datasets: [{
+                                label: '圖表',
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                xAxes: [{
+                                            display: true,
+                                            scaleLabel: {
+                                                display: true,
+                                                labelString: '時間軸'
+                                            }
+                                        }],
+                                        yAxes: [{
+                                            display: true,
+                                            scaleLabel: {
+                                                display: true,
+                                                labelString: '值'
+                                            }
+                                        }]
+                            }
+                        }
+                    });
+
+                    let evtSource = new EventSource("/chartEventStream", {withCredentials: true});
+                        evtSource.onmessage = function (e) {
+                            let serverData = JSON.parse(e.data);
+                            console.log('EventData:- ', serverData);
+
+                            myChart.data.labels.push(serverData.time);
+                            myChart.data.datasets[0].data.push(serverData.value);
+                            myChart.update();
+                        };
+                    </script>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
